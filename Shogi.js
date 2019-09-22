6acd0fc570dfe80d64c0ac7ee9b876594e816001
nana.k <77nanakuroki@gmail.com>
2019-09-22 19:12:43 +0900

    全ての駒を作りました

--- Shogi.js
+++ Shogi.js
@@ -31,49 +31,24 @@ Vue.component('ban',{
 
 Vue.component('ou', {
   template:
-  '<polygon @click="move($event)" :points="points" stroke = "black" fill = "wheat"></polygon>',
-  // '<text x="0" y="30" fill="blue">本日は晴天なり</text>',
+    '<g @click="move()":transform="trans"><polygon  :points="points" stroke = "black" fill = "wheat"></polygon><text x="50" y="50" font-size="35">玉</text></g>',
   props: ["x","y","teban"],
   computed:{
+    trans: function(){
+  	    return "translate(" + parseInt(this.x)*100 + "," + parseInt(this.y)*100 + ")";
+  	},
     points:function(){
       if (this.teban==0){
-        pstr ="";
-        pstr +=(5+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-        pstr +=(10+parseInt(this.x)*100+" ");
-        pstr +=(70+parseInt(this.y)*100+" ");
-        pstr +=(50+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        pstr +=(90+parseInt(this.x)*100+" ");
-        pstr +=(70+parseInt(this.y)*100+" ");
-        pstr +=(95+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-        // return "5 0 10 70 50 100 90 70 95 0";
-        return pstr;
+         return "5 0 10 70 50 100 90 70 95 0";
       }
       else {
-        pstr ="";
-        pstr +=(5+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        pstr +=(10+parseInt(this.x)*100+" ");
-        pstr +=(30+parseInt(this.y)*100+" ");
-        pstr +=(50+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-
-        pstr +=(90+parseInt(this.x)*100+" ");
-        pstr +=(30+parseInt(this.y)*100+" ");
-
-        pstr +=(95+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        // return "5 100 10 30 50 0 90 30 95 100";
-        return pstr;
+         return "5 100 10 30 50 0 90 30 95 100";
       }
     }
   },
   methods:{
     move:function(event){
-      //alert (event.offsetX);
-      // this.searchBlock(event);
+
       currentX = parseInt(event.offsetX/100);
      currentY  = parseInt(event.offsetY/100);
       //alert (currentX + " " +currentY);
@@ -92,42 +67,18 @@ alert(number);
 
 Vue.component('gin', {
   template:
-  '<polygon @click="move($event)" :points="points" stroke = "black" fill = "wheat"></polygon>',
-  // '<text x="0" y="30" fill="blue">本日は晴天なり</text>',
-  props: ["x","y","teban"],
+  '<g @click="move()":transform="trans"><polygon  :points="points" stroke = "black" fill = "wheat"></polygon><text x="27" y="70" font-size="45">銀</text></g>',
+props: ["x","y","teban"],
   computed:{
+    trans: function(){
+        return "translate(" + parseInt(this.x)*100 + "," + parseInt(this.y)*100 + ")";
+    },
     points:function(){
       if (this.teban==0){
-        pstr ="";
-        pstr +=(5+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-        pstr +=(10+parseInt(this.x)*100+" ");
-        pstr +=(70+parseInt(this.y)*100+" ");
-        pstr +=(50+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        pstr +=(90+parseInt(this.x)*100+" ");
-        pstr +=(70+parseInt(this.y)*100+" ");
-        pstr +=(95+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-        // return "5 0 10 70 50 100 90 70 95 0";
-        return pstr;
+         return "5 0 10 70 50 100 90 70 95 0";
       }
       else {
-        pstr ="";
-        pstr +=(5+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        pstr +=(10+parseInt(this.x)*100+" ");
-        pstr +=(30+parseInt(this.y)*100+" ");
-        pstr +=(50+parseInt(this.x)*100+" ");
-        pstr +=(parseInt(this.y)*100+" ");
-
-        pstr +=(90+parseInt(this.x)*100+" ");
-        pstr +=(30+parseInt(this.y)*100+" ");
-
-        pstr +=(95+parseInt(this.x)*100+" ");
-        pstr +=(100+parseInt(this.y)*100+" ");
-        // return "5 100 10 30 50 0 90 30 95 100";
-        return pstr;
+         return "5 100 10 30 50 0 90 30 95 100";
       }
     }
   },
